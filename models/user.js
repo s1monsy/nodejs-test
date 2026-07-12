5a5b9a29712d9ef34b4a21c50035ef95b8eca7db
DenysPolupan <polupan2004denis@gmail.com>
2026-07-12 17:23:22 +0300

    Implement add-to-cart

--- models/user.js
+++ models/user.js
@@ -4,9 +4,30 @@ const getDb = require("../util/database").getDb;
 const ObjectId = mongodb.ObjectId;
 
 class User {
-  constructor(username, email) {
+  constructor(username, email, cart, id) {
     this.name = username;
     this.email = email;
+    this.cart = cart;
+    this._id = id;
+  }
+
+  addToCart(product) {
+    const cartProductIndex = this.cart.items.findIndex((cp) => {
+      return cp.productId === product._id;
+    });
+
+    let newQuantity = 1;
+
+    if (cartProductIndex >= 0) {
+      newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+    }
+
+    const updatedCart = { items: [{ productId: new ObjectId(product.id), quantity: newQuantity }] };
+
+    const db = getDb();
+    return db
+      .collection("users")
+      .updateOne({ _id: new ObjectId(this._id) }, { $set: { cart: updatedCart } });
   }
 
   save() {
