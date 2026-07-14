fd88e3cee3e1905f84f05cd9982328cd4cfb3614
DenysPolupan <polupan2004denis@gmail.com>
2026-07-14 17:36:07 +0300

    Migrate orders/cart to Mongoose

--- models/user.js
+++ models/user.js
@@ -22,7 +22,42 @@ const userSchema = new Schema({
   },
 });
 
-module.exports = mongoose.model("User", userSchema);
+userSchema.methods.addToCart = function (product) {
+  const cartProductIndex = this.cart.items.findIndex((cp) => {
+    return cp.productId.toString() === product._id.toString();
+  });
+
+  let newQuantity = 1;
+  const updatedCartItems = [...this.cart.items];
+
+  if (cartProductIndex >= 0) {
+    newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+    updatedCartItems[cartProductIndex].quantity = newQuantity;
+  } else {
+    updatedCartItems.push({ productId: product.id, quantity: newQuantity });
+  }
+
+  const updatedCart = { items: updatedCartItems };
+
+  this.cart = updatedCart;
+  return this.save();
+};
+
+user.Schema.methods.removeFromCart = function (productId) {
+  const updatedCartItems = this.cart.items.filter((item) => {
+    return item.productId !== productId.toString();
+  });
+
+  this.cart.items = updatedCartItems;
+  return this.save();
+};
+
+userSchema.methods.clearCart = function () {
+  this.cart = { items: [] };
+  return this.save();
+};
+
+odule.exports = mongoose.model("User", userSchema);
 
 // const mongodb = require("mongodb");
 // const getDb = require("../util/database").getDb;
