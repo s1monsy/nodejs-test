eba53b680881600efc56cac253a86e829642a415
DenysPolupan <polupan2004denis@gmail.com>
2026-07-17 14:05:05 +0300

    Add session-based auth with MongoDB store

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,15 +1,29 @@
-exports.getLogin = (req, res, next) => {
-  const isLoggedIn = req.get("Cookie").split(";")[1].trim().split("=")[1] === "true";
+const User = require('../models/user');
 
-  res.render("auth/login", {
-    path: "/login",
-    pageTitle: "Login",
-    isAuthenticated: isLoggedin,
+exports.getLogin = (req, res, next) => {
+  res.render('auth/login', {
+    path: '/login',
+    pageTitle: 'Login',
+    isAuthenticated: false
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  req.setHeader("Set-Cookie", "loggedIn=true; HttpOnly");
+  User.findById('5bab316ce0a7c75f783cb8a8')
+    .then(user => {
+      req.session.isLoggedIn = true;
+      req.session.user = user;
+      req.session.save(err => {
+        console.log(err);
+        res.redirect('/');
+      });
+    })
+    .catch(err => console.log(err));
+};
 
-  res.redirect("/");
+exports.postLogout = (req, res, next) => {
+  req.session.destroy(err => {
+    console.log(err);
+    res.redirect('/');
+  });
 };
